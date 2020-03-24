aa36e00f7484bc7230743454652cf6e01d73a39d
Lizhixuxu <2128879209@qq.com>
2020-03-24 23:44:06 +0800

    数据库语句

--- src/main/resources/db/migration/V10__Create_notification_table.sql
+++ src/main/resources/db/migration/V10__Create_notification_table.sql
@@ -6,5 +6,5 @@ create table notification
     outerid    bigint        not null,
     type       int           not null,
     gmt_create bigint        not null,
-    status     int default 0 not null,
+    status     int default 0 not null
 );
